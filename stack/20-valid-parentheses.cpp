d4a87d66f84e0f8dc956d87d92da43886d3c48cc
Marwan Sabry <112923720+MarwanVII@users.noreply.github.com>
2024-01-02 00:17:48 +0200

    Update 20-valid-parentheses.cpp

--- stack/20-valid-parentheses.cpp
+++ stack/20-valid-parentheses.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto i:s)
+        {
+            if(i == '(' || i == '{' || i == '[' || st.empty())
+                st.push(i);
+            else
+            {
+                if(i == ')' && st.top() == '(')
+                    st.pop();
+                else if(i == '}' && st.top() == '{')
+                    st.pop();
+                else if(i == ']' && st.top() == '[')
+                    st.pop();
+                else
+                    st.push(i);
+            }
+        }
+        return st.empty();
+    }
+};
